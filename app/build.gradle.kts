ac282929fb95649b6271a53b344b4e2011b77bdd
GFO <gia.focxverashvili@gmail.com>
2025-01-26 10:19:27 +0400

    add dependencies and packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.appmovies"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.appmovies"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +51,46 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.0")
+
+    // Lottie
+    implementation("com.airbnb.android:lottie:6.3.0")
+
+    // Koin
+    implementation("io.insert-koin:koin-android:4.0.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Fragments integration
+    val navVersion = "2.8.4"
+    implementation("androidx.navigation:navigation-fragment:$navVersion")
+    implementation("androidx.navigation:navigation-ui:$navVersion")
+
+    // Room Database
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion") // Kotlin Symbol Processing - KSP
+
+    // View Model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+
+    // Youtube dependencies
+    implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:11.1.0")
+
+    // Circular image dependencies
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    // Paging library
+    val paging_version = "3.3.5"
+    implementation("androidx.paging:paging-runtime:$paging_version")
 }
